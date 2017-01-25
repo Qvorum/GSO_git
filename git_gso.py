ed3c3087cf69780cecfa79e374e29708abc154c9
Qvorum <hakonha213@gmail.com>
2017-01-25 15:57:02 +0000

    Skrifar í skránni og lokar henni

--- git_gso.py
+++ git_gso.py
@@ -1,4 +1,7 @@
 #Hákon Haraldsson
 #GSÖ2
 #25.1.2017
-f = open("texti.txt","w+")          #Býr til textaskjal
+texti = input("Hvað viltu að skráin heiti? ")
+y = open(texti +".txt","w+")
+y.write("Gert af Hákoni Haraldssyni")       #Skrifar texta
+y.close()                                   #Lokar skránni
